c48d0cf00fcc3c851911c8eb458f4a875549285e
Diego Escalante <diego.escalante01@gmail.com>
2020-11-14 23:43:10 -0700

    Add barrel sprite and animation

--- Assets/Scripts/FuelRefillBehavior.cs
+++ Assets/Scripts/FuelRefillBehavior.cs
@@ -2,32 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider), typeof(MeshRenderer))]
+[RequireComponent(typeof(BoxCollider))]
 public class FuelRefillBehavior : MonoBehaviour {
+
+    public Sprite filledSprite;
+    public Sprite emptySprite;
+    public GameObject blobShadow;
 
     public float respawnTime = 5f;
     public float fillAmountPercentage = 1f;
     private BoxCollider playerColl, coll;
     private float respawnTimeLeft;
-    private MeshRenderer rend;
+    private SpriteRenderer rend;
 
     private void Start() {
         playerColl = GameObject.FindGameObjectWithTag("Player").GetComponent<BoxCollider>();
         coll = GetComponent<BoxCollider>();
-        rend = GetComponent<MeshRenderer>();
+        rend = transform.Find("Sprite").GetComponent<SpriteRenderer>();
     }
 
     private void Update() {
-        if (rend.enabled && coll.Overlaps(playerColl)) {
+        if (rend.sprite == filledSprite && coll.Overlaps(playerColl)) {
             // If available and touched by player, refill their fuel, disable.
             playerColl.GetComponent<PlayerHovering>().Refuel(fillAmountPercentage);
-            rend.enabled = false;
+            rend.sprite = emptySprite;
+            blobShadow.SetActive(false);
             respawnTimeLeft = respawnTime;
-        } else if (!rend.enabled) {
+        } else if (rend.sprite == emptySprite) {
             // If not enabled, tick down and respawn when ready.
             respawnTimeLeft -= Time.deltaTime;
             if (respawnTimeLeft <= 0) {
-                rend.enabled = true;
+                rend.sprite = filledSprite;
+                blobShadow.SetActive(true);
             }
         }
     }
