23c014a398a027a44e314c58b29f79fa1af7b62d
Diego Escalante <diego.escalante01@gmail.com>
2020-11-28 19:14:15 -0700

    Add skybox

---
+++ Assets/Scripts/SkyboxRotator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxRotator : MonoBehaviour {
+    
+    public float speed = 0.2f;
+    private Material skyMat;
+
+    private void Start() {
+        skyMat = RenderSettings.skybox;
+    }
+
+    void Update(){
+        skyMat.SetFloat("_Rotation", skyMat.GetFloat("_Rotation") + Time.deltaTime * speed);
+    }
+}
